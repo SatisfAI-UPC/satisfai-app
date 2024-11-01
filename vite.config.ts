c967873eca8cbe00bac6ffe84f767928f8b0800d
sayufu <lilianafuye@gmail.com>
2024-11-01 18:25:51 -0500

    feat: add .env

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-  ],
+    plugins: [
+        react(),
+    ],
+    esbuild: {
+        loader: "tsx",
+        // Skip type checking on build
+        ignoreAnnotations: true
+    },
+    build: {
+        target: 'esnext',
+        minify: false,     // Skips minification to reduce complexity
+    },
 })
