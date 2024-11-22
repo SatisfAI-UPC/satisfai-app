23a7e0cb51e540b877ced7b6c8b66102bfcb7a7f
sayufu <lilianafuye@gmail.com>
2024-11-22 02:57:31 -0500

    feat: add AI insight

--- src/survey/services/OpenAIService.ts
+++ src/survey/services/OpenAIService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { globalApiUrl } from "../../shared/services/GlobalAPI.ts";
+import {globalApiUrl, surveyApiUrl} from "../../shared/services/GlobalAPI.ts";
 
 export function generateOpenAISurveyQuestions(userPrompt: string, companyId: number) {
     return axios.get(`${globalApiUrl}/openai/api/generate-survey`, {
@@ -11,5 +11,14 @@ export function generateOpenAISurveyQuestions(userPrompt: string, companyId: num
         .then((response) => response.data)
         .catch((error) => {
             throw new Error(error.response?.data?.message || "Error generating survey questions");
+        });
+}
+
+
+export function generateOpenAISurveyResponsesRecommendation(surveyId: string | number) {
+    return axios.post(`${surveyApiUrl}/${surveyId}/generate-recommendation`)
+        .then((response) => response.data)
+        .catch((error) => {
+            throw new Error(error.response?.data?.message || "Error generating survey recommendation");
         });
 }
